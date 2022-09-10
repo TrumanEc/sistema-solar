60cb47994d8adce02b7984c8f81c13353cd764d0
Darwin Esteban CIfuentes <78879151+TrumanEc@users.noreply.github.com>
2022-09-10 18:52:53 -0500

    Update sistem.js

--- sistem.js
+++ sistem.js
@@ -1,3 +1,17 @@
+window.requestAnimFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
+
+
 var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
 
